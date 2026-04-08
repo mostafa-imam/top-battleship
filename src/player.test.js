ffb7c390863d2773857a2f74d1408f637b1b9182
Mostafa Imam <137308525+mostafa-imam@users.noreply.github.com>
2026-04-09 00:28:05 +0200

    feat: Add tests for Player class

---
+++ src/player.test.js
@@ -0,0 +1,10 @@
+import { Player } from "./player.js";
+import { test, describe, expect } from "@jest/globals";
+
+describe("Player constructor", () => {
+  test("player name is returned correctly", () => {
+    const player = new Player("John");
+
+    expect(player.name).toBe("John");
+  });
+});
