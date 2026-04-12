4b6632a2eb987ceea64f9b991a5e7de72235cbc0
Mostafa Imam <137308525+mostafa-imam@users.noreply.github.com>
2026-04-12 16:50:56 +0200

    feat: Add methods to render dom elements

---
+++ src/render-dom.js
@@ -0,0 +1,81 @@
+export { renderGrid, renderBox, renderShips };
+
+function renderGrid(player) {
+    let gameboardContainer;
+    if (player.name !== "computer") {
+        gameboardContainer = document.querySelector(`.${player.name}-gameboard`)
+    } else {
+        gameboardContainer = document.querySelector(`.computer-gameboard`)
+    }
+
+    gameboardContainer.textContent = '';
+
+    for (let i = 1; i <= 10; i++) {
+        for (let j = 1; j <= 10; j++) {
+            let div = document.createElement("div");
+            div.classList.add("box");
+
+            if (player.name !== "computer") {
+                div.classList.add(`${player.name}-box`);
+            } else {
+                div.classList.add(`computer-box`);
+            }
+
+            div.dataset.x = i;
+            div.dataset.y = j;
+
+            gameboardContainer.appendChild(div)
+        }
+    }
+}
+
+function renderShips(player) {
+
+    const ships = player.gameboard.shipsPlacements;
+    let coordinates = [];
+    ships.forEach(ship => coordinates.push(ship.coordinates))
+    const coordinatesArr = coordinates.flat();
+
+    let boxes;
+
+    if (player.name !== 'computer') {
+        boxes = document.querySelectorAll(".player-box");
+    } else {
+        boxes = document.querySelectorAll(".computer-box");
+    }
+
+    for (let box of boxes) {
+        for (let coo of coordinatesArr) {
+            let cooX = coo[0];
+            let cooY = coo[1];
+
+            let boxX = +box.dataset.x;
+            let boxY = +box.dataset.y;
+
+            if (cooX === boxX && cooY === boxY) {
+                box.classList.add("ship-on-grid");
+            }
+        }
+    }
+}
+
+function renderBox(player, target) {
+    const ships = player.gameboard.shipsPlacements;
+    let coordinates = [];
+    ships.forEach(ship => coordinates.push(ship.coordinates))
+    const coordinatesArr = coordinates.flat();
+
+    let x = +target.dataset.x;
+    let y = +target.dataset.y;
+
+    for (let coo of coordinatesArr) {
+        if (coo[0] === x && coo[1] === y) {
+            target.classList.remove("ship-on-grid");
+            target.classList.add("hit");
+            return
+        }
+    }
+
+    target.classList.remove("ship-on-grid");
+    target.classList.add("miss");
+}
